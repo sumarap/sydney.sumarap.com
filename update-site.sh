f1ae81ea7cb1fd7491b5ae2e77a51f63f145d37d
Keith Peterson <keithpe@gmail.com>
2016-09-19 22:19:01 -0700

    Add support for table/row layout of last row of index pictures.

--- update-site.sh
+++ update-site.sh
@@ -1,3 +1,4 @@
 # Use this to upload web site files.
-aws s3 cp . s3://sydney-keithpe8935 --recursive --exclude ".git/*" --exclude "*.sh" --exclude ".DS*"
+#aws s3api create-bucket --bucket sydney.sumarap.com --region us-west-2 --create-bucket-configuration LocationConstraint=us-west-2
+aws s3 cp . s3://sydney.sumarap.com --recursive --exclude ".git/*" --exclude "*.sh" --exclude ".DS*"
 
