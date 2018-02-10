1f37ca87cc052f78739f794ae5cbd51ed1631e0f
sumarap <keithpe@gmail.com>
2018-02-10 05:51:41 -0800

    Added support for copying *.js files.

--- update-html.sh
+++ update-html.sh
@@ -1,3 +1,3 @@
 # Use this to upload web site files.
-aws s3 cp . s3://sydney.sumarap.com --recursive --exclude "*.*" --include "*.html"
+aws s3 cp . s3://sydney.sumarap.com --recursive --exclude "*.*" --include "*.html" --include "*.js"
 
