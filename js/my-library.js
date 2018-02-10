6841a937456d37cf93486983f7689cb83800f433
sumarap <keithpe@gmail.com>
2018-02-10 08:28:17 -0800

    modified my-library.js to only add class to the div elements that already contain either the col-sm-3 or col-lg-12 class.

--- js/my-library.js
+++ js/my-library.js
@@ -1,5 +1,9 @@
 function Change2VerticalFormat(image_id){
     //console.log('executing Change2VerticalFormat()');
-    $("div.col-sm-3").removeClass("col-sm-3").addClass("col-lg-12");
+    if ( $("div").hasClass("col-sm-3") ){
+        $("div.col-sm-3").removeClass("col-sm-3").addClass("col-lg-12");
+    } else if ( $("div").hasClass("col-lg-12") ) {
+        $("div.col-lg-12").removeClass("col-lg-12").addClass("col-sm-3");
+    }
     window.location.href = "#"+image_id;
 }
