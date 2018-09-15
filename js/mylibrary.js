922adbac2a64a5dcda3544f83747381722dba6f2
sumarap <keithpe@gmail.com>
2018-09-15 00:07:30 -0400

    Add support for displaying spinning css loader before images are loaded into index.html

---
+++ js/mylibrary.js
@@ -0,0 +1,17 @@
+function Change2VerticalFormat(image_id){
+    //console.log('executing Change2VerticalFormat()');
+    if ( $("div").hasClass("col-sm-3") ){
+        $("div.col-sm-3").removeClass("col-sm-3").addClass("col-lg-12");
+    } else if ( $("div").hasClass("col-lg-12") ) {
+        $("div.col-lg-12").removeClass("col-lg-12").addClass("col-sm-3");
+    }
+    window.location.href = "#"+image_id;
+}
+
+function imageLoaded() {
+  // Turn off loader image
+  document.getElementById("loaderCase").style.display = 'none';
+  document.getElementById("loader").style.display = 'none';
+  // Turn on div with loaded images
+  document.getElementById("content").style.display = 'block';
+}
