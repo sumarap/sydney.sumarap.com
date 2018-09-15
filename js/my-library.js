8305821846b1547df9f8e72e01d68ca051815174
sumarap <keithpe@gmail.com>
2018-09-15 00:15:59 -0400

    Renamed js/my-library.js to js/myLibrary.js, outside git, so removing js/my-library.js here.

--- js/my-library.js
+++
@@ -1,9 +0,0 @@
-function Change2VerticalFormat(image_id){
-    //console.log('executing Change2VerticalFormat()');
-    if ( $("div").hasClass("col-sm-3") ){
-        $("div.col-sm-3").removeClass("col-sm-3").addClass("col-lg-12");
-    } else if ( $("div").hasClass("col-lg-12") ) {
-        $("div.col-lg-12").removeClass("col-lg-12").addClass("col-sm-3");
-    }
-    window.location.href = "#"+image_id;
-}
