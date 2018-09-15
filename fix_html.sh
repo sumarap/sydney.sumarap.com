53f678fd8a6046c58ca3e186fa3133521bbc3afc
sumarap <keithpe@gmail.com>
2018-09-15 00:09:36 -0400

    Add sed commands to use thumbnails (_tn) instead of full size images.

---
+++ fix_html.sh
@@ -0,0 +1,17 @@
+# Used this script to change html files to use thumbnail (_tn) images instead of full size.
+sed -i 0 's/.jpg/_tn.jpg/g' 2016_Parent_Weekend.html
+sed -i 0 's/.jpg/_tn.jpg/g' 2015_Fairbanks.html
+sed -i 0 's/.jpg/_tn.jpg/g' 2016_Aug_Mariners_Red_Sox.html
+sed -i 0 's/.jpg/_tn.jpg/g' 2016_August_Olympic.html
+sed -i 0 's/.jpg/_tn.jpg/g' 2016_August_Seafair.html
+sed -i 0 's/.jpg/_tn.jpg/g' 2016_Back_From_College.html
+sed -i 0 's/.jpg/_tn.jpg/g' birthday.html
+sed -i 0 's/.jpg/_tn.jpg/g' cakes.html
+sed -i 0 's/.jpg/_tn.jpg/g' christmas1998.html
+sed -i 0 's/.jpg/_tn.jpg/g' disney_world.html
+sed -i 0 's/.jpg/_tn.jpg/g' july1999.html
+sed -i 0 's/.jpg/_tn.jpg/g' may1999.html
+sed -i 0 's/.jpg/_tn.jpg/g' playground.html
+sed -i 0 's/.jpg/_tn.jpg/g' port_townsend.html
+sed -i 0 's/.jpg/_tn.jpg/g' tulips1999.html
+
