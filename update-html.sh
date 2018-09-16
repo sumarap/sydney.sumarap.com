b282028e5e81807849e269edc9582c1bfd9f5c43
sumarap <keithpe@gmail.com>
2018-09-16 11:49:50 -0400

    Separated aws cp commands into separate lines

--- update-html.sh
+++ update-html.sh
@@ -1,3 +1,17 @@
 # Use this to upload web site files.
-aws s3 cp . s3://sydney.sumarap.com --recursive --exclude "*.*" --include "*.html" --include "*.js" --include "*.css"
+
+# Copy html files
+aws s3 cp . s3://sydney.sumarap.com --recursive --include "*.html" --exclude "dev-playground/*" --exclude "*.sh" --exclude ".DS_Store" --exclude ".git/*" --exclude "css/*" --exclude "fonts/*" --exclude "images/*" --exclude "js/*" --exclude "logs/*"
+
+# Copy js files
+aws s3 cp js/ s3://sydney.sumarap.com/js/ --recursive 
+
+# Copy css files
+aws s3 cp css/ s3://sydney.sumarap.com/css/ --recursive 
+
+# Copy font files
+#aws s3 cp fonts/ s3://sydney.sumarap.com/fonts/ --recursive 
+
+# Copy images files
+#aws s3 cp images/ s3://sydney.sumarap.com/images/ --recursive 
 
